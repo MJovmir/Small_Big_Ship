e69cba1ae4286a4ac6eaa9364c1a5dd970909111
MJovmir <116758799+MJovmir@users.noreply.github.com>
2022-11-24 20:18:25 +0100

    Add files via upload
    this code is a home work for:
    https://github.com/dorinesinenco/EDUQATION/blob/master/programming/python/control/for.ship.sea.ro.md

---
+++ ship.py
@@ -0,0 +1,35 @@
+# SHIP 
+from os import system
+
+
+
+
+small_ship = 5
+big_ship = 5
+while True:
+    system("cls")
+    print("\n")
+                 
+    for x in range(1,11):
+                if x == small_ship:
+                    print( "W", end="" )
+                    if small_ship == big_ship:
+                        print("wWw", end="" )
+                else:
+                 print( "~", end="" )
+                 
+                 
+    print("\n")       
+    option = input(">>> ")      
+    # small_ship movement
+    if option == "a" and small_ship > 1:  # move left
+                small_ship -= 1
+
+    if option == "d" and small_ship < 10:  # move right
+                small_ship += 1      
+        
+    print("\n")
+    if option == "x" or small_ship == 0 or small_ship == 10:  # exit
+        system("cls")
+        print("The Ship arrived! Congratulations!! ")
+        break
